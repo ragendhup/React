f27c845d808412d016a09279f4d0110597cfb6b9
ragendhup <ragendhup18@gmail.com>
2024-10-07 20:08:06 +0530

    First commit

--- dbit/src/App.js
+++ dbit/src/App.js
@@ -1,9 +1,9 @@
-
 import './App.css';
-import Changestate from './Reactprog/Changestate';
+//import Changestate from './Reactprog/Changestate';
 //import Secondprog from './Reactprog/Secondprog';
 //import Greet from './Reactprog/Greet';
 //import Welcome from './Reactprog/Welcome';
+import Httpreq from'./Reactprog/Httpreq';
 function App() {
   return (
     <div className="App">
@@ -11,7 +11,15 @@ function App() {
       <Greet name="Pavi" designation="Developer"/>*/}
       {/*<Welcome/>*/}
       {/*<Event/>*/}
-      <Changestate/>
+      {/*<Changestate/>*/}
+      {/* <Firstprog/>
+<Greet name="raj" designation="Developer"/>
+<Greet name="rani" designation-"tester"/> */}
+{/* <Welcome name=" 896 students" branch=" CSE"/>*/}
+{/* <Event/>*/}
+{/* <ExampUseeffect/> */}
+<Httpreq/>
+
     </div>
   );
 }
